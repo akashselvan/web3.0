39a564a6fbb42165431ec7770c7d79c86dd52b98
unknown <tamilsp2909@gmail.com>
2021-12-19 09:28:55 +0530

    import checked

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Login from '../components/Login/Login';
+import Login from '../components/login/login';
 import { useMoralis } from 'react-moralis';
 import FirstPage from '../components/home/FirstPage'
 
